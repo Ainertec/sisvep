32e522c30b2510e119d61fd76533bf19a476ba4c
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-04-11 22:59:57 -0300

    upload inicial 1

---
+++ frontend/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    icon: './img/icon.ico',
+    autoHideMenuBar: true,
+    title: 'Sisvep - sistema para gerenciamento de vendas',
+    minHeight:840,
+    minWidth: 1300
+  })
+
+  mainWindow.loadFile('index.html')
+  mainWindow.maximize()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+}
+
+
+app.on('ready', createWindow)
+
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
