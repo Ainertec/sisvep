1b0d57347499dc753a87b881462d531d0adf226c
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-04-16 23:58:21 -0300

    atualizacao funcoes extras venda

--- frontend/classes/Venda.js
+++ frontend/classes/Venda.js
@@ -3,15 +3,29 @@
 ///---------------------------------------------------------Tela principal de venda-------------------------------------------------------
 
 
+
+
+//vetor com códigos dos produtos da venda
+var vetorCodigoItensVenda=[];
+
+
+
+
+
 //funcao responsavel pela autenticacao de usuario no setor venda
-function autenticacaoVenda(){
+function autenticacaoVendaFacede(){
 
     if(1==1){
+        vetorCodigoItensVenda=[];
         document.getElementById('janela2').innerHTML = telaVenda();
-        carregarDadosItensVenda();
     }
 
 }
+
+
+
+
+
 
 
 // funcao responsavel por criar a tela de vendas
@@ -25,13 +39,7 @@ function telaVenda(){
                 codigoHTML+='<div class="card bg-success border-danger" style="margin:5px; color: #fff">'
                     codigoHTML+='<div class="card-header"><strong>Valor Total</strong></div>'    
                     codigoHTML+='<div class="card-body">'
-                        codigoHTML+='<h2><strong>R$ 1200.00</strong></h2>'
-                    codigoHTML+='</div>'
-                codigoHTML+='</div>'
-
-                codigoHTML+='<div class="card border-danger" style="margin:5px;">'
-                    codigoHTML+='<div class="card-body">'
-                        codigoHTML+='<h5><strong>Código Nº: 123456789123</strong></h5>'
+                        codigoHTML+='<h2><strong>R$ <span id="exibirValorTotalAtualizado">0.00</span></strong></h2>'
                     codigoHTML+='</div>'
                 codigoHTML+='</div>'
 
@@ -39,14 +47,23 @@ function telaVenda(){
                     codigoHTML+='<div class="card-body">'
                         codigoHTML+='<div class="form-group row">'
                             codigoHTML+='<label for="qtdItemDaVenda">Produto <strong>X</strong></label>' 
-                        codigoHTML+='<input type="Number" class="form-control form-control-sm col-3" id="qtdItemDaVenda" style="margin-left:10px" value=2>'
+                            codigoHTML+='<input type="Number" class="form-control form-control-sm col-3" id="qtdItemDaVenda" style="margin-left:10px" value=1>'
                         codigoHTML+='</div>'
                         codigoHTML+='<div class="form-group row">'
-                            codigoHTML+='<div name="campoebuttondeleteitemvenda" class="input-group mb-3">'
+                            codigoHTML+='<div class="input-group mb-3">'
                                 codigoHTML+='<label for="campoebuttondeleteitemvenda">Produto</label>'
-                                codigoHTML+='<input type="Number" class="form-control form-control-sm col-4" style="margin-left:10px" placeholder="Código" aria-describedby="buttonDeleteItemDaVenda">'
+                                codigoHTML+='<input id="campocodigodeleteitemvenda" type="Number" class="form-control form-control-sm col-4" style="margin-left:10px" placeholder="Código" aria-describedby="buttonDeleteItemDaVenda">'
                                 codigoHTML+='<div class="input-group-append">'
-                                    codigoHTML+='<button class="btn btn-danger btn-sm" type="button" id="buttonDeleteItemDaVenda"><span class="fas fa-times iconsTam"></span> Remover</button>'
+                                    codigoHTML+='<button onclick="removerProdutoDaLista(document.getElementById(\'campocodigodeleteitemvenda\').value);" class="btn btn-danger btn-sm" type="button" id="buttonDeleteItemDaVenda"><span class="fas fa-times iconsTam"></span> Remover</button>'
+                                codigoHTML+='</div>'
+                            codigoHTML+='</div>'
+                        codigoHTML+='</div>'
+                        codigoHTML+='<div class="form-group row">'
+                            codigoHTML+='<div class="input-group mb-3">'
+                                codigoHTML+='<label for="campocodigoadicionaritemvenda">Produto</label>'
+                                codigoHTML+='<input id="campocodigoadicionaritemvenda" type="Number" class="form-control form-control-sm col-4" style="margin-left:10px" placeholder="Código" aria-describedby="buttonAdicionarItemDaVenda">'
+                                codigoHTML+='<div class="input-group-append">'
+                                    codigoHTML+='<button onclick="buscarProduto(document.getElementById(\'campocodigoadicionaritemvenda\').value);" class="btn btn-primary btn-sm" type="button" id="buttonAdicionarItemDaVenda"><span class="fas fa-plus iconsTam"></span> Adicionar</button>'
                                 codigoHTML+='</div>'
                             codigoHTML+='</div>'
                         codigoHTML+='</div>'
@@ -57,8 +74,8 @@ function telaVenda(){
                     codigoHTML+='<div class="card-header"><strong>Pagamento</strong></div>'    
                     codigoHTML+='<div class="card-body">'
                         codigoHTML+='<div class="btn-group btn-lg btn-block" role="group" aria-label="Basic example">'
-                            codigoHTML+='<button type="button" class="btn btn-primary"><span class="fas fa-dollar-sign iconsTam"></span> Dinheiro</button>'
-                            codigoHTML+='<button type="button" class="btn btn-info"><span class="fas fa-credit-card iconsTam"></span> Cartão</button>'
+                            codigoHTML+='<button onclick="modalPagamento(\'dinheiro\');" type="button" class="btn btn-primary"><span class="fas fa-dollar-sign iconsTam"></span> Dinheiro</button>'
+                            codigoHTML+='<button onclick="modalPagamento(\'cartao\');" type="button" class="btn btn-info"><span class="fas fa-credit-card iconsTam"></span> Cartão</button>'
                         codigoHTML+='</div>'
                     codigoHTML+='</div>'
                 codigoHTML+='</div>'
@@ -88,39 +105,270 @@ function telaVenda(){
 }
 
 
+
+
+
+
+
 //funcao reponsavel por carregar a lista e exibir os dados do item da venda
-function carregarDadosItensVenda(){
+function carregarDadosItensVenda(json){
 
-    var codigoHTML='', codigoHTML2='', cont=0;
-        
-    while(cont<20){
+    var codigoHTML='', codigoHTML2='';
 
-        codigoHTML2+='<table class="table table-danger">'
-            codigoHTML2+='<tbody>'
-                codigoHTML2+='<tr>'
-                    codigoHTML2+='<td>Código: Mark</td>'
-                    codigoHTML2+='<td>Nome: fjdhdhf</td>'
-                    codigoHTML2+='<td>Preço Uni.: R$ 20.00</td>'
-                    codigoHTML2+='<td>Quantidade: 20</td>'
-                codigoHTML2+='</tr>'
-            codigoHTML2+='</tbody>'
-        codigoHTML2+='</table>'
+    if(document.getElementById('codigoProduto'+json.codigo) != null){
+
+        document.getElementById('quantidadeProduto'+json.codigo).innerHTML=parseInt($('#quantidadeProduto'+json.codigo).text()) + parseInt($('#qtdItemDaVenda').val());
+        document.getElementById('valorProduto'+json.codigo).innerHTML=(parseFloat($('#valorProduto'+json.codigo).text()) + parseFloat((json.preco * $('#qtdItemDaVenda').val()))).toFixed(2);
+    
+    }else{
+
+    vetorCodigoItensVenda.push(json.codigo);
+
+    codigoHTML2+='<table class="table table-danger">'
+        codigoHTML2+='<tbody>'
+            codigoHTML2+='<tr>'
+                codigoHTML2+='<th>Código: '+json.codigo+'</th>'
+                codigoHTML2+='<th>Nome: '+json.nome+'</th>'
+                codigoHTML2+='<th>Preço Uni.: R$ '+(json.preco).toFixed(2)+'</th>'
+                codigoHTML2+='<th>Quantidade: '+$('#qtdItemDaVenda').val()+'</th>'
+            codigoHTML2+='</tr>'
+        codigoHTML2+='</tbody>'
+    codigoHTML2+='</table>'
 
 
-        codigoHTML+='<tr>'
-            codigoHTML+='<td>123456789123</td>'
-            codigoHTML+='<td>fjdhdhf</td>'
-            codigoHTML+='<td>'+(20*$('#qtdItemDaVenda').val())+'</td>'
-            codigoHTML+='<td>'+$('#qtdItemDaVenda').val()+'</td>'
-        codigoHTML+='</tr>'
+    codigoHTML+='<tr id="produto-'+json.codigo+'">'
+        codigoHTML+='<td id="codigoProduto'+json.codigo+'">'+json.codigo+'</td>'
+        codigoHTML+='<td id="nomeProduto'+json.codigo+'">'+json.nome+'</td>'
+        codigoHTML+='<td id="valorProduto'+json.codigo+'">'+(json.preco*$('#qtdItemDaVenda').val()).toFixed(2)+'</td>'
+        codigoHTML+='<td id="quantidadeProduto'+json.codigo+'">'+$('#qtdItemDaVenda').val()+'</td>'
+    codigoHTML+='</tr>'
 
-        cont++;
 
-        document.getElementById('dadosItemVenda').innerHTML = codigoHTML2;
-        $('#tabelaCarregarItensParaVenda').append(codigoHTML);
-        
-        codigoHTML='';
-        codigoHTML2='';
+    document.getElementById('dadosItemVenda').innerHTML = codigoHTML2;
+    $('#tabelaCarregarItensParaVenda').append(codigoHTML);
+
     }
+
+    gerarValorTotal();
+
+}
+
+
+
+
+
+
+//funcao responsavel por gerar o modal de pagamento
+function modalPagamento(tipo){
+
+    var codigoHTML='';
+
+    codigoHTML+='<div class="modal fade" id="modalPagamento" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">'
+        codigoHTML+='<div class="modal-dialog" role="document">'
+            codigoHTML+='<div class="modal-content">'
+                codigoHTML+='<div class="modal-header">'
+
+                    if(tipo=='dinheiro'){
+                        codigoHTML+='<h5 class="modal-title">Pagamento Dinheiro</h5>'
+                    }else if(tipo=='cartao'){
+                        codigoHTML+='<h5 class="modal-title">Pagamento Cartão</h5>'
+                    }
+
+                codigoHTML+='</div>'
+                codigoHTML+='<div class="modal-body">'
+                    codigoHTML+='<div class="card bg-success border-danger" style="margin:5px;">'
+                        codigoHTML+='<div class="card-body">'
+                            codigoHTML+='<h2 class="text-light"><strong>Valor Total: R$ <span id="valorTotalPagamento">'+$('#exibirValorTotalAtualizado').text()+'</span></strong></h2>'
+                        codigoHTML+='</div>'
+                    codigoHTML+='</div>'
+
+                    if(tipo=='dinheiro'){
+                        codigoHTML+='<div class="card bg-danger border-danger color-ligth" style="margin:5px;">'
+                            codigoHTML+='<div class="card-body">'
+                                codigoHTML+='<h2 class="text-light"><strong>Troco: R$ <span id="exibirTroco"></span></strong></h2>'
+                            codigoHTML+='</div>'
+                        codigoHTML+='</div>'
+                        codigoHTML+='<label for="valorPago" style="margin-top:40px;"><strong>Valor Pago</strong></label>' 
+                        codigoHTML+='<input id="valorPago" type="Number" class="form-control form-control-lg col-5" style="margin-left:10px">'
+                    }
+
+                codigoHTML+='</div>'
+                codigoHTML+='<div class="modal-footer">'
+                    codigoHTML+='<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>'
+                    
+                    if(tipo=='dinheiro'){
+                        codigoHTML+='<button onclick="cadastrarVenda(\'Dinheiro\');" type="button" class="btn btn-primary" data-dismiss="modal">Efetuar Pagamento</button>'
+                    }else if(tipo=='cartao'){
+                        codigoHTML+='<button onclick="cadastrarVenda(\'Cartão\');" type="button" class="btn btn-primary" data-dismiss="modal">Efetuar Pagamento</button>'
+                    }
+
+                codigoHTML+='</div>'
+            codigoHTML+='</div>'
+        codigoHTML+='</div>'
+    codigoHTML+='</div>'
+ 
+    document.getElementById('modal').innerHTML = codigoHTML;
+
+    $('#modalPagamento').modal('show');
+
+    if(tipo=='dinheiro'){
+        $("#valorPago").keypress(function(){
+
+            setTimeout(function(){
+                document.getElementById('exibirTroco').innerHTML = (parseFloat(document.getElementById('valorPago').value) - parseFloat($('#valorTotalPagamento').text())).toFixed(2);
+            }, 1000);
+    
+        });
+    }
+
+}
+
+
+
+
+
+//funcao responsavel por retirar o produto da lista
+function removerProdutoDaLista(codigoProduto){
+
+    if(document.getElementById('produto-'+codigoProduto) != null){
+        vetorCodigoItensVenda.forEach(function (item, indice, array) {
+            if(codigoProduto==item){
+                vetorCodigoItensVenda[indice]=-1;
+            }
+          });
+        document.getElementById('produto-'+codigoProduto).innerHTML='';
+        gerarValorTotal();
+    }else{
+        mensagemDeErro('Código de barras inválido!');
+    }
+
+}
+
+
+
+
+
+
+//funcao responsavel por gerar e apresentar o valor total
+function gerarValorTotal(){
+
+    var cont=0, valorTotal=0;
+
+    while(vetorCodigoItensVenda[cont]){
+        if(document.getElementById('valorProduto'+vetorCodigoItensVenda[cont]) != null){
+            valorTotal+=parseFloat($('#valorProduto'+vetorCodigoItensVenda[cont]).text());
+        }
+        cont++;
+    }
+
+    document.getElementById('exibirValorTotalAtualizado').innerHTML = valorTotal.toFixed(2);
+}
+
+
+
+
+
+
+//funcao responsavel por buscar e enviar os itens para a lista
+function buscarProduto(codigo){
+    
+    var json = JSON.parse('{"codigo":'+codigo+', "nome":"produto1", "preco":23.30}');
+    
+    carregarDadosItensVenda(json);
+
+    beepAlerta();
+
+}
+
+
+
+
+
+
+
+
+//funcao responsavel por emitir um som no momento em que o código de barras chega
+function beepAlerta(){
+
+    let beep = new AudioContext(),
+    alerta = beep.createOscillator();
+
+    alerta.type = 'triangle';
+    alerta.connect(beep.destination);
+    alerta.start();
+
+    setTimeout(function(){alerta.stop();},100);
+}
+
+
+
+
+
+
+
+
+//funcao responsavel por receber o codigo de barras lido pelo celular em real time
+function socketCodigoBarrasRealTime(){
+    
+    if ("WebSocket" in window) {
+
+        alert("WebSocket is supported by your Browser!");
+        var ws = new WebSocket("ws://localhost:9998/echo");
+         
+       /* ws.onopen = function() {
+           ws.send("Message to send");
+           alert("Message is sent...");
+        };*/
+         
+        ws.onmessage = function (evt) { 
+           var received_msg = evt.data;
+           buscarProduto(received_msg);
+           alert("Message is received...");
+        };
+         
+        ws.onclose = function() { 
+           alert("Connection is closed..."); 
+        };
+
+     } else {
+        alert("WebSocket NOT supported by your Browser!");
+     }
+
+}
+
+
+
+
+
+
+
+//funcao responsavel por cadastrar a venda após concluida
+function cadastrarVenda(formaPagamento){
+    
+    var datad = new Date();
+    var json = '{"dataHora":"'+datad.getFullYear()+'-'+(datad.getMonth()+1)+'-'+datad.getDate()+' '+datad.getHours()+':'+(datad.getMinutes()+1)+':'+(datad.getSeconds()+1)+'", "formaPagamento":"'+formaPagamento+'", "valorTotal":'+$('#exibirValorTotalAtualizado').text()+', "listaItens":[';
+    var aux=true;
+
+
+    vetorCodigoItensVenda.forEach(function (item) {
+        if(document.getElementById('codigoProduto'+item) != null){
+            if(aux){
+                json+='{"nome":"'+$('#nomeProduto'+item).text()+'" , "quantidade":'+parseInt($('#quantidadeProduto'+item).text())+' , "precoUni":'+ (parseFloat($('#valorProduto'+item).text()) / parseInt($('#quantidadeProduto'+item).text())).toFixed(2) +'}'
+                aux=false;
+            }else{
+                json+=',{"nome":"'+$('#nomeProduto'+item).text()+'" , "quantidade":'+parseInt($('#quantidadeProduto'+item).text())+' , "precoUni":'+ (parseFloat($('#valorProduto'+item).text()) / parseInt($('#quantidadeProduto'+item).text())).toFixed(2) +'}'
+            }
+        }
+    });
+
+    json+=']}'
+    
+    alert(json);
+
+    json = JSON.parse(json);
+
+    alert('Cadastrado com sucesso!');
+
+    setTimeout(function(){autenticacaoVendaFacede();},1000);
 
 }
