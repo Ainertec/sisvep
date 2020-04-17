b56373d0b0ebcda8938bc338228a015f3c8c05f4
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-04-17 14:48:35 -0300

    teste

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,15 @@ const routes = require('./routes');
 
 const app = express();
 
+const url = process.env.NODE_ENV === 'test' ? process.env.MONGO_URL : process.env.DATABASE_URL;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.use(express.json());
 app.use(routes);
 app.use(errors());
