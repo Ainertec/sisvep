3491e80c8e1ffa02d94f6431021f1b090e45353e
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-04-23 20:21:27 -0300

    Atualizacao funcionalidades basicas

---
+++ frontend/classes/AtalhosTeclas.js
@@ -0,0 +1,11 @@
+//teclas de atalho do software
+
+Mousetrap.bind('ctrl+1', function() { autenticacaoVendaFacede(); });
+Mousetrap.bind('ctrl+2', function() { subMenuProduto(); });
+Mousetrap.bind('ctrl+3', function() { autenticacaoFornecedorFacede(); });
+Mousetrap.bind('ctrl+4', function() { autenticacaoFuncionarioFacede(); });
+Mousetrap.bind('ctrl+5', function() { autenticacaoEstoqueFacede(); });
+Mousetrap.bind('ctrl+6', function() { autenticacaoRelatorioFacede(); });
+Mousetrap.bind('ctrl+7', function() { subMenuImpressao(); });
+Mousetrap.bind('ctrl+8', function() { autenticacaoLoja(); });
+Mousetrap.bind('esc', function() { window.location.href = "./home.html"; });
--- frontend/classes/Funcionario.js
+++ frontend/classes/Funcionario.js
@@ -9,6 +9,17 @@
 
 
 
+//vetor responsavel por guardar os funcionarios selecionados
+var VETORDEFUNCIONARIOCLASSEFUNCIONARIO = [];
+
+
+
+
+
+
+
+
+
 
 //funcao responsavel pela autenticacao no setor de funcionario
 function autenticacaoFuncionarioFacede(){
@@ -17,9 +28,9 @@ function autenticacaoFuncionarioFacede(){
     
     if(JSON.parse(situacao).tipo == 'Administrador'){
         document.getElementById('janela2').innerHTML = telaFuncionario('Admin');
-        document.getElementById('listaFuncionarios').innerHTML = carregarListaFuncionario();
     }else if(JSON.parse(situacao).tipo == 'Comum'){
         document.getElementById('janela2').innerHTML = telaFuncionario('Comum');
+        buscarFuncionario('Comum');
     }else{
         mensagemDeErro("Usuário não autorizado!");
     }
@@ -51,8 +62,8 @@ function telaFuncionario(tipo){
                 codigoHTML+='<input id="buscaProduto" type="text" class="form-control" placeholder="Nome">'
             codigoHTML+='</div>'
             codigoHTML+='<div class="btn-group btn-lg btn-block" role="group">'
-                codigoHTML+='<button type="button" class="btn btn-outline-primary"><span class="fas fa-search"></span> Buscar por Nome</button>'
-                codigoHTML+='<button type="button" class="btn btn-outline-primary"><span class="fas fa-search"></span> Exibir todos</button>'
+                codigoHTML+='<button onclick="buscarFuncionario(\'Administrador\');" type="button" class="btn btn-outline-primary"><span class="fas fa-search"></span> Buscar por Nome</button>'
+                codigoHTML+='<button onclick="buscarFuncionario(\'Administrador\');" type="button" class="btn btn-outline-primary"><span class="fas fa-search"></span> Exibir todos</button>'
             codigoHTML+='</div>'
         codigoHTML+='</div>'
     }
@@ -97,8 +108,8 @@ function telaFuncionario(tipo){
                     }else if(tipo=='Admin'){
                         codigoHTML+='<select class="custom-select mr-sm-6" id="tipoFun">'
                     }
-                        codigoHTML+='<option value="1">Comum</option>'
-                        codigoHTML+='<option value="2">Administrador</option>'
+                        codigoHTML+='<option value="Comum">Comum</option>'
+                        codigoHTML+='<option value="Administrador">Administrador</option>'
                     codigoHTML+='</select>'
                 codigoHTML+='</div>'
                 codigoHTML+='<div class="form-group col-md-6">'
@@ -107,10 +118,10 @@ function telaFuncionario(tipo){
                 codigoHTML+='</div>'
             codigoHTML+='</div>'
             codigoHTML+='<div class="form-row">'
-                codigoHTML+='<button type="button" class="btn btn-success" style="margin: 5px;"><span class="fas fa-edit"></span> Atualizar</button>'
+                codigoHTML+='<button onclick="atualizarFuncionario();" type="button" class="btn btn-success" style="margin: 5px;"><span class="fas fa-edit"></span> Atualizar</button>'
                 if(tipo=='Admin'){
-                    codigoHTML+='<button type="button" class="btn btn-primary" style="margin: 5px;"><span class="fas fa-save"></span> Cadastrar</button>'
-                    codigoHTML+='<button type="button" class="btn btn-danger" style="margin: 5px;"><span class="fas fa-trash-alt"></span> Excluir</button>'
+                    codigoHTML+='<button onclick="cadastrarFuncionario();" type="button" class="btn btn-primary" style="margin: 5px;"><span class="fas fa-save"></span> Cadastrar</button>'
+                    codigoHTML+='<button onclick="excluirFuncionario();" type="button" class="btn btn-danger" style="margin: 5px;"><span class="fas fa-trash-alt"></span> Excluir</button>'
                 }
             codigoHTML+='</div>'
         codigoHTML+='</form>'
@@ -131,21 +142,159 @@ function telaFuncionario(tipo){
 
 
 //funcao responsavel por carregar lista com os funcionarios pesquisados
-function carregarListaFuncionario(){
+function carregarListaFuncionario(json,posicao){
 
     var codigoHTML='';
 
-    for(var cont=0; cont<10; cont++){
-        codigoHTML+='<a href="#" class="list-group-item list-group-item-action">'
-            codigoHTML+='<div class="d-flex w-100 justify-content-between">'
-                codigoHTML+='<h5 class="mb-1">Nome: </h5>'
-                codigoHTML+='<small>Tipo: </small>'
-            codigoHTML+='</div>'
-            codigoHTML+='<small>Descrição: </small>'
-        codigoHTML+='</a>'
-    }
+    codigoHTML+='<a onclick="carregarDadosFuncionario('+posicao+');" href="#" class="list-group-item list-group-item-action">'
+        codigoHTML+='<div class="d-flex w-100 justify-content-between">'
+            codigoHTML+='<h5 class="mb-1">Nome: '+json.nome+'</h5>'
+            codigoHTML+='<small>Tipo: '+json.tipofun+'</small>'
+        codigoHTML+='</div>'
+    codigoHTML+='</a>'
 
 
     return codigoHTML;
+
+}
+
+
+
+
+
+
+
+
+
+
+//funcao responsavel por carregar os dados do funcionario selecionado
+function carregarDadosFuncionario(posicao){
+
+    document.getElementById('id').value = VETORDEFUNCIONARIOCLASSEFUNCIONARIO[posicao].id;
+    document.getElementById('nome').value = VETORDEFUNCIONARIOCLASSEFUNCIONARIO[posicao].nome;
+    document.getElementById('login').value = VETORDEFUNCIONARIOCLASSEFUNCIONARIO[posicao].login;
+    document.getElementById('senha').value = VETORDEFUNCIONARIOCLASSEFUNCIONARIO[posicao].senha;
+    document.getElementById('tipoFun').value = VETORDEFUNCIONARIOCLASSEFUNCIONARIO[posicao].tipofun;
+    document.getElementById('email').value = VETORDEFUNCIONARIOCLASSEFUNCIONARIO[posicao].email;
+
+}
+
+
+
+
+
+
+
+
+
+
+//funcao responsavel por buscar os funcionarios
+function buscarFuncionario(tipo){
+
+    VETORDEFUNCIONARIOCLASSEFUNCIONARIO=[];
+
+    if(tipo=='Administrador'){
+
+        var json = '[{"id":"1a2","nome":"Funcionario 1","login":"fun 1","senha":"123","tipofun":"Administrador","email":"fun1@gmail.com"},{"id":"2b3","nome":"Funcionario 2","login":"fun 2","senha":"123","tipofun":"Comum","email":"fun2@gmail.com"}]', cont=0;
+
+        json = JSON.parse(json);
+
+        document.getElementById('listaFuncionarios').innerHTML='';
+
+        while(json[0]){
+            VETORDEFUNCIONARIOCLASSEFUNCIONARIO.push(json[cont]);
+            $('#listaFuncionarios').append(carregarListaFuncionario(json[cont],cont));
+            cont++;
+        }
+
+    }else if(tipo=='Comum'){
+        
+        var json = '{"id":"3c4","nome":"Funcionario 3","login":"fun 3","senha":"123","tipofun":"Comum","email":"fun3@gmail.com"}'
+
+        json = JSON.parse(json);
+
+        VETORDEFUNCIONARIOCLASSEFUNCIONARIO.push(json);
+        carregarDadosFuncionario(0);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//funcao responsavel por cadastrar funcionario
+function cadastrarFuncionario(){
+
+    if(validaDadosCampo(['#nome','#login','#senha','#tipofun','#email'])){
+        
+        var json='{"nome":"'+$('#nome').val()+'",'
+                json+='"login":"'+$('#login').val()+'",'
+                json+='"senha":"'+$('#senha').val()+'",'
+                json+='"tipofun":"'+$('#tipoFun').val()+'",'
+                json+='"email":"'+$('#email').val()+'"}'
+        
+        document.getElementById('janela2').innerHTML = json;
+
+    }else{
+        mensagemDeErro('Preencha todos os campos!');
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+//funcao responsavel por atualizar funcionario
+function atualizarFuncionario(){
+    
+    if(validaDadosCampo(['#id','#nome','#login','#senha','#tipofun','#email'])){
+        
+        var json='{"id":"'+$('#id').val()+'",'
+                json+='"nome":"'+$('#nome').val()+'",'
+                json+='"login":"'+$('#login').val()+'",'
+                json+='"senha":"'+$('#senha').val()+'",'
+                json+='"tipofun":"'+$('#tipoFun').val()+'",'
+                json+='"email":"'+$('#email').val()+'"}'
+        
+        document.getElementById('janela2').innerHTML = json;
+
+    }else{
+        mensagemDeErro('Preencha todos os campos!');
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+//funcao responsavel por excluir o funcionario
+function excluirFuncionario(){
+    
+    if(validaDadosCampo(['#id'])){
+        
+        var json = '{"id":"'+$('#id').val()+'"}'
+        document.getElementById('janela2').innerHTML = json;
+
+    }else{
+        mensagemDeErro('Não foi possivel, falta de ID!');
+    }
 
 }
