9f2cb109ec88cda9cdd0e26a1dccb5db18f528c2
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-05-18 09:10:40 -0300

    Refactoring

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
-// import {  } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Icon } from 'react-native-elements'
 
@@ -15,209 +15,106 @@ import Configuracao from './pages/Configuracao'
 const Drawer = createDrawerNavigator()
 const Stack = createStackNavigator()
 
-const DrawerPages = () => {
+const StackOptions = () => {
+  const navigation = useNavigation()
+  return {
+    headerTitleAlign: 'center',
+    headerTitle: 'Sisvep',
+    headerTintColor: 'white',
+    headerStyle: { backgroundColor: '#222' },
+    headerLeft: () => (
+      <Icon
+        size={26}
+        name='more-vert'
+        color='#fff'
+        onPress={() => navigation.openDrawer()}
+      />
+    ),
+  }
+}
+
+function TabRest() {
   return (
-    <Drawer.Navigator
-      initialRouteName='Home'
-      drawerType='back'
-      drawerStyle={{
-        backgroundColor: '#222',
-        width: 240,
-      }}
-      drawerContentOptions={{
-        activeTintColor: '#999',
-        labelStyle: { color: '#fff' },
-      }}
-    >
-      <Drawer.Screen name='Home' component={RestScren} />
-      <Drawer.Screen name='Venda' component={LeitorCodBarras} />
-      <Drawer.Screen name='Cadastro' component={CreateProduct} />
-      <Drawer.Screen name='Atualizar' component={SeachUpdate} />
-      <Drawer.Screen name='Busca' component={Search} />
-      <Drawer.Screen name='Configurações' component={Configuracao} />
-    </Drawer.Navigator>
+    <Stack.Navigator screenOptions={StackOptions}>
+      <Stack.Screen name='Home' component={RestScren} />
+    </Stack.Navigator>
   )
 }
 
-const Routes = () => {
+function TabVenda() {
   return (
-    <Stack.Navigator
-      initialRouteName='Home'
-      screenOptions={{
-        headerTitle: 'Sisvep',
-        headerTintColor: 'white',
-        headerStyle: { backgroundColor: '#222221' },
-        headerRight: () => (
-          <Icon name='more-vert' color='#fff' onPress={() => {}} />
-        ),
-      }}
-    >
+    <Stack.Navigator screenOptions={StackOptions}>
+      <Stack.Screen name='Venda' component={LeitorCodBarras} />
+    </Stack.Navigator>
+  )
+}
+
+function TabCreateProduct() {
+  return (
+    <Stack.Navigator screenOptions={StackOptions}>
+      <Stack.Screen name='Cadastro' component={CreateProduct} />
+    </Stack.Navigator>
+  )
+}
+
+function TabSearch() {
+  return (
+    <Stack.Navigator screenOptions={StackOptions}>
+      <Stack.Screen name='Busca' component={Search} />
+    </Stack.Navigator>
+  )
+}
+
+function TabUpdateStock() {
+  return (
+    <Stack.Navigator>
       <Stack.Screen
-        options={{
-          headerBackTitleVisible: false,
-          headerLeft: false,
-        }}
-        name='Home'
-        component={DrawerPages}
+        name='Busca'
+        component={SeachUpdate}
+        options={StackOptions}
       />
       <Stack.Screen
-        options={{
-          headerBackTitleVisible: false,
-          headerLeft: false,
-        }}
-        name='UpdateStock'
+        name='Atualização'
         component={UpdateStock}
+        options={{
+          headerTitleAlign: 'center',
+          headerTitle: 'Sisvep',
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: '#222' },
+        }}
       />
     </Stack.Navigator>
   )
 }
 
-// function TabAguardo({ navigation }) {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTintColor: 'white',
-//         headerStyle: { backgroundColor: '#222' },
-//         headerRight: () => (
-//           <Icon
-//             name='more-vert'
-//             color='#fff'
-//             onPress={() => navigation.openDrawer()}
-//           />
-//         ),
-//       }}
-//     >
-//       <Stack.Screen name='Sisvep' component={RestScren} />
-//     </Stack.Navigator>
-//   )
-// }
+function TabConfiguracao() {
+  return (
+    <Stack.Navigator screenOptions={StackOptions}>
+      <Stack.Screen name='Configuração' component={Configuracao} />
+    </Stack.Navigator>
+  )
+}
 
-// function TabVenda({ navigation }) {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTintColor: 'white',
-//         headerStyle: { backgroundColor: '#000' },
-//         headerRight: () => (
-//           <Icon
-//             name='more-vert'
-//             color='#fff'
-//             onPress={() => navigation.openDrawer()}
-//           />
-//         ),
-//       }}
-//     >
-//       <Stack.Screen name='Sisvep' component={LeitorCodBarras} />
-//     </Stack.Navigator>
-//   )
-// }
-
-// function TabCreateProduct({ navigation }) {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTintColor: 'white',
-//         headerStyle: { backgroundColor: '#222' },
-//         headerRight: () => (
-//           <Icon
-//             name='more-vert'
-//             color='#fff'
-//             onPress={() => navigation.openDrawer()}
-//           />
-//         ),
-//       }}
-//     >
-//       <Stack.Screen name='Sisvep' component={CreateProduct} />
-//     </Stack.Navigator>
-//   )
-// }
-
-// function TabSearch({ navigation }) {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTintColor: 'white',
-//         headerStyle: { backgroundColor: '#222' },
-//         headerRight: () => (
-//           <Icon
-//             name='more-vert'
-//             color='#fff'
-//             onPress={() => navigation.openDrawer()}
-//           />
-//         ),
-//       }}
-//     >
-//       <Stack.Screen name='Sisvep' component={Search} />
-//     </Stack.Navigator>
-//   )
-// }
-
-// function TabUpdateStock({ navigation }) {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTintColor: 'white',
-//         headerStyle: { backgroundColor: '#222' },
-//         headerRight: () => (
-//           <Icon
-//             name='more-vert'
-//             color='#fff'
-//             onPress={() => navigation.openDrawer()}
-//           />
-//         ),
-//       }}
-//     >
-//       <Stack.Screen name='Sisvep' component={UpdateStock} />
-//     </Stack.Navigator>
-//   )
-// }
-
-// function TabConfiguracao({ navigation }) {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTintColor: 'white',
-//         headerStyle: { backgroundColor: '#222' },
-//         headerRight: () => (
-//           <Icon
-//             name='more-vert'
-//             color='#fff'
-//             onPress={() => navigation.openDrawer()}
-//           />
-//         ),
-//       }}
-//     >
-//       <Stack.Screen name='Sisvep' component={Configuracao} />
-//     </Stack.Navigator>
-//   )
-// }
-
-// const DrawerRoutes = (
-//   <NavigationContainer>
-//     <Drawer.Navigator
-//       initialRouteName='Home'
-//       drawerType='back'
-//       drawerStyle={{
-//         backgroundColor: '#222',
-//         width: 240,
-//       }}
-//       drawerContentOptions={{
-//         activeTintColor: '#999',
-//         labelStyle: { color: '#fff' },
-//       }}
-//     >
-//       <Drawer.Screen name='Tela de Descanso' component={TabAguardo} />
-//       <Drawer.Screen name='Venda' component={TabVenda} />
-//       <Drawer.Screen name='Cadastrar produto' component={TabCreateProduct} />
-//       <Drawer.Screen name='Buscar produto' component={TabSearch} />
-//       <Drawer.Screen name='Estoque' component={TabUpdateStock} />
-//       <Drawer.Screen name='Configuração' component={TabConfiguracao} />
-//     </Drawer.Navigator>
-//   </NavigationContainer>
-// )
-
-// const Routes = (
-
-// )
+const Routes = () => (
+  <Drawer.Navigator
+    initialRouteName='Home'
+    drawerType='back'
+    drawerStyle={{
+      backgroundColor: '#222',
+      width: 240,
+    }}
+    drawerContentOptions={{
+      activeTintColor: '#999',
+      labelStyle: { color: '#fff' },
+    }}
+  >
+    <Drawer.Screen name='Home' component={TabRest} />
+    <Drawer.Screen name='Venda' component={TabVenda} />
+    <Drawer.Screen name='Cadastrar produto' component={TabCreateProduct} />
+    <Drawer.Screen name='Buscar produto' component={TabSearch} />
+    <Drawer.Screen name='Estoque' component={TabUpdateStock} />
+    <Drawer.Screen name='Configuração' component={TabConfiguracao} />
+  </Drawer.Navigator>
+)
 
 export default Routes
