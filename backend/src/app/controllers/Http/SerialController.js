cc360343f822b41899c63756d6fb0141bc5ade1c
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-06-02 15:01:38 -0300

    correcao de serial

--- backend/src/app/controllers/Http/SerialController.js
+++ backend/src/app/controllers/Http/SerialController.js
@@ -2,7 +2,7 @@ module.exports = {
   destroy(req, res) {
     const { password } = req.query;
 
-    if (password === 52164521655455362) {
+    if (password === "52164521655455362") {
       process.exit(0);
       return req.status(200).send();
     }
